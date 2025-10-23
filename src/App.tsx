59debe5909c4373c3e7f046dcb867dcf13fcf666
ppravinkumar <pravinniva@gmail.com>
2025-10-23 16:08:32 +0530

    Updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import ManualDeployment from './pages/ManualDeployment'
-import ManualDeploymentCreate from './pages/ManualDeploymentCreate'
 import AutonomousDeployment from './pages/AutonomousDeployment'
 import Policy from './pages/Policy'
 
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/deployment/manual" replace />} />
           <Route path="/deployment/manual" element={<ManualDeployment />} />
-          <Route path="/deployment/manual/create" element={<ManualDeploymentCreate />} />
           <Route path="/deployment/autonomous" element={<AutonomousDeployment />} />
           <Route path="/policy" element={<Policy />} />
         </Routes>
--- src/pages/ManualDeployment.tsx
+++ src/pages/ManualDeployment.tsx
@@ -1,14 +1,7 @@
-import { useNavigate } from 'react-router-dom'
 import PlaceholderCard from '../components/PlaceholderCard'
 import styles from './Page.module.css'
 
 function ManualDeployment() {
-  const navigate = useNavigate()
-
-  const handleCreateDeployment = () => {
-    navigate('/deployment/manual/create')
-  }
-
   return (
     <div className={styles.page}>
       <div className={styles.header}>
@@ -19,7 +12,7 @@ function ManualDeployment() {
         title="No manual deployments yet"
         primaryButtonText="Create Manual Deployment"
         secondaryLinkText="Learn more"
-        onPrimaryClick={handleCreateDeployment}
+        onPrimaryClick={() => console.log('Create manual deployment')}
         onSecondaryClick={() => console.log('Learn more')}
       />
     </div>
